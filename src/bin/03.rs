b2d2b531dad95a213d73756d3cd9d9eaa3a0d16b
Sleeyax <yourd3veloper@gmail.com>
2024-12-04 17:52:54 +0100

    day 3: try to solve part 2 (wrong answer)

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,23 +1,31 @@
-use regex::Regex;
+use fancy_regex::Regex;
 
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<i32> {
-    let regex = Regex::new(r"(?m)mul\((\d+),(\d+)\)").unwrap();
+fn mul(input: &str, regex: Regex) -> Option<i32> {
     let total = regex
         .captures_iter(input)
         .map(|m| {
-            let (_, [x, y]) = m.extract();
-            let x = x.parse::<i32>().unwrap();
-            let y = y.parse::<i32>().unwrap();
+            let captures = m.unwrap();
+            let x = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
+            let y = captures.get(2).unwrap().as_str().parse::<i32>().unwrap();
             x * y
         })
         .sum::<i32>();
     Some(total)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+    mul(input, Regex::new(r"(?m)mul\((\d+),(\d+)\)").unwrap())
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    // TODO: this results in a wrong answer... idk why...
+    mul(
+        // the start of the input is enabled, so prepend a 'do()' instruction so that the regex matches.
+        &format!("{}{}", "do()", input),
+        Regex::new(r"(?m)do\(\)(?:(?!don't\(\)).)*?mul\((\d+),(\d+)\)").unwrap(),
+    )
 }
 
 #[cfg(test)]
@@ -32,7 +40,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
     }
 }
